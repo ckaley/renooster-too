2815a1badead20296eb26a3ad0266d395a12b0dd
Ann Karpinski <annkarpinski@gmail.com>
2020-08-26 19:37:36 -0500

    changed configuration of navbar and footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -24,48 +24,13 @@ const Footer = ({ profile }) => {
 
     return (
         <footer className="page-footer">
-            <div className="container">
-                <div className="row">
-                    <div className="col s12 m6">
-                        <h5 className="white-text">{fullName}</h5>
-                        <p className="grey-text text-lighten-4">{headline}</p>
-                    </div>
-                    <div className="col s12 m4 offset-m2">
-                        <div className="row">
-                            <div className="col s12">
-                                <h5 className="white-text">Skills</h5>
-                            </div>
-                            <div className="col s3 m6 l4">
-                                <ul>
-                                    {skills2D[0] && skills2D[0].map((skill, index) => <li key={index}>{skill}</li>)}
-                                </ul>
-                            </div>
-                            <div className="col s3 m6 l4">
-                                <ul>
-                                    {skills2D[1] && skills2D[1].map((skill, index) => <li key={index}>{skill}</li>)}
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className="footer-copyright">
                 <div className="container">
                     <div className="row">
                         <div className="col s12 m6">
-                            <span className="fuck">{`© Copyright ${new Date().getFullYear()}`}</span>
-                        </div>
-                        <div className="col s12 m4 offset-m2">
-                            <a href={github} rel="noopener noreferrer" target="_blank">
-                                <FontAwesomeIcon icon={faGithubSquare} />
-                            </a>
-                            <a href={linkedIn} rel="noopener noreferrer" target="_blank">
-                                <FontAwesomeIcon icon={faLinkedin} />
-                            </a>
+                            <span className="copyright">{`© Copyright ${new Date().getFullYear()}`}</span>
                         </div>
                     </div>
                 </div>
-            </div>
         </footer>
     )
 }
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,26 +1,57 @@
 // dependencies
-import React from 'react'
-import { Link, NavLink } from 'react-router-dom';
+import React from "react";
+import { Link, NavLink, useLocation } from "react-router-dom";
 
 const Navbar = ({ profile }) => {
-    // // destructure profile
-    const { fullName } = profile
+  // // destructure profile
+  const { fullName } = profile;
 
+  // react router hook variable
+  let location = useLocation();
+  console.log(location.pathname);
+
+  if (location.pathname === "/" || location.pathname === "/signup") {
+    return null;
+  } else {
     return (
-        <nav>
-            <div className="container">
-                <div className="row">
-                    <div className="col s12">
-                        <Link to="/" className="brand-logo">{fullName}</Link>
-                        <ul id="nav-mobile" className="right hide-on-med-and-down">
-                            <li><NavLink exact to="/">Portfolio</NavLink></li>
-                            <li><NavLink exact to="/contact">Contact</NavLink></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </nav>
-    )
-}
+      <nav className="navbar">
+        <div className="row">
+          <div className="col s12">
+            <Link to="/" className="brand-logo">
+              {fullName}
+            </Link>
+            <ul id="nav-mobile" className="right hide-on-med-and-down">
+              <li>
+                <NavLink exact to="/subscriptions">
+                  Subscriptions
+                </NavLink>
+              </li>
+              <li>
+                <NavLink exact to="/add">
+                  Add
+                </NavLink>
+              </li>
+              <li>
+                <NavLink exact to="/expiring">
+                  Expiring Soon
+                </NavLink>
+              </li>
+              <li>
+                <NavLink exact to="/contact">
+                  Contact
+                </NavLink>
+              </li>
+              <li>
+                <NavLink exact to="/">
+                  Sign Out
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+};
 
-export default Navbar
+export default Navbar;
