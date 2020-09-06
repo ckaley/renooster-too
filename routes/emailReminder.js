95de4dad9c6804c51e71b610d79bafbbdfa5d923
Craig Kaley <craig@caucusroom.com>
2020-09-06 15:06:13 -0600

    Updating

--- routes/emailReminder.js
+++ routes/emailReminder.js
@@ -1,5 +1,6 @@
 // dependencies
 var express = require("express");
+var nodemailer = require("nodemailer");
 var db = require("../models");
 const { Mongoose } = require("mongoose");
 
@@ -16,11 +17,8 @@ router.get("/", (req, res) => {
   // Define expired Subsciptions Array
   const expiredSubscriptions = [];
 
-  // Retrieve Subscriptions that are do to expire in 30 days or less
-  // db.Subscription.find({ endDate: { $lte: filterDate } })
-  //   .then((subscriptions) => res.status(200).json(subscriptions))
-  //   .catch((err) => res.status(404).json(err));
-
+  // Retrieve Subscriptions that are do to expire in 30 days
+  // or less and also lookup the user asscoiated with the subscription
   db.Subscription.aggregate([
     {
       $match: {
@@ -32,11 +30,88 @@ router.get("/", (req, res) => {
         from: "profiles",
         localField: "profileID",
         foreignField: "_id",
-        as: "users",
+        as: "user",
       },
     },
   ])
-    .then((subscriptions) => res.status(200).json(subscriptions))
+    .then((expiredSubscriptions) => {
+      console.log("I am sending emails!");
+
+      var firstName = "";
+      var lastName = "";
+      var email = "";
+      var expSubscription = "";
+      var expireDate = "";
+
+      // create nodemailer transport
+      var transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+          user: process.env.NODEMAILER_USER,
+          pass: process.env.NODEMAILER_PASS,
+        },
+      });
+
+      for (var i = 0; i < expiredSubscriptions.length; i++) {
+        // set values
+        firstName = expiredSubscriptions[i].user[0].firstName;
+        lastName = expiredSubscriptions[i].user[0].lastName;
+        email = expiredSubscriptions[i].user[0].email;
+        expSubscription = expiredSubscriptions[i].name;
+        expireDate = expiredSubscriptions[i].endDate;
+
+        // define mail options
+        var mailOptions = {
+          to: process.env.EMAIL,
+          subject: "You have a Subscription that is about to expire! ",
+          text:
+            firstName +
+            " " +
+            lastName +
+            ", " +
+            " | " +
+            "Your Subscription to " +
+            expSubscription +
+            " expires on " +
+            expireDate +
+            " | " +
+            "This is a courtesy reminder for you to renew your subscription!" +
+            " | " +
+            "Thank you," +
+            " | " +
+            "The Renooster Team",
+          html:
+            "<p>" +
+            firstName +
+            " " +
+            lastName +
+            ",</p>" +
+            "<p></p>" +
+            "<p>Your Subscription to " +
+            expSubscription +
+            " expires on " +
+            expireDate +
+            "</p>" +
+            "<p></p>" +
+            "<p></p>" +
+            "<p>This is a courtesy reminder for you to renew your subscription!</p>" +
+            "<p></p>" +
+            "<p>Thank you,<p>" +
+            "<p></p>" +
+            "<p>The Renooster Team</p>",
+        };
+
+        // send mail
+        transporter.sendMail(mailOptions, (err, result) => {
+          if (err) {
+            console.log("falied to send message");
+          }
+          console.log("Message Sent");
+        });
+      }
+
+      res.status(200).json(expiredSubscriptions);
+    })
     .catch((err) => res.status(404).json(err));
 });
 
